6a26326a5f5f1507da6ddacd5fa8c08dd000e358
Sabrina Baggett <sabrinabaggett@administrators-MacBook-Pro.local>
2026-03-18 23:31:06 -0700

    parsing cookies and displaying as list issue #5

--- src/elements/cork-cookie-manager.js
+++ src/elements/cork-cookie-manager.js
@@ -9,6 +9,9 @@ export default class CorkCookieManager extends Mixin(LitElement)
 
   static get properties() {
     return {
+        groupRules: {type: Array, attribute: 'group-rules'},
+        parentDomain: {type: String, attribute: 'parent-domain'},
+        config: {type: Object},
     }
   }
 
@@ -19,9 +22,39 @@ export default class CorkCookieManager extends Mixin(LitElement)
   constructor() {
     super();
     this.render = render.bind(this);
+    this.cookies = [];
+    this.config = {
+        name: "all",
+        label: "All non-HttpOnly cookies",
+        patterns: [".*"]
+    };
   }
 
+  /**
+   * @description Retrieves all cookies accessible via JavaScript (i.e., non-HttpOnly cookies) and stores them in the `cookies` property. 
+   * To manage HttpOnly cookies, you would need to do so from the server side by sending appropriate Set-Cookie headers.
+   */
+  getCookies() {
+    let allCookies = document.cookie;
+    const ca = allCookies.split(';');
+    const cookies = ca.map(cookie => {
+        const [name, value] = cookie.split('=').map(c => c.trim());
+        return { name:name, value:value, valueLength: value.length };
+    });
+    this.cookies = cookies;
+ }
+
+  /**
+   * @param {string} cookieName - The name of the cookie to delete
+   * @returns {void}
+   * Deletes a cookie by setting its expiration date to a past date. 
+   * To delete an HttpOnly cookie, you would need to do so from the server side by sending a Set-Cookie header with an expired date.
+   */
+ deleteCookie(cookie) {
+    console.log(`Deleting cookie: ${cookie.name}`);
+ }
   connectedCallback() {
+    this.getCookies();
     super.connectedCallback();
   }
 
--- src/elements/cork-cookie-manager.tpl.js
+++ src/elements/cork-cookie-manager.tpl.js
@@ -1,19 +1,61 @@
 import {html, css} from 'lit';
+import baseStyles from "@ucd-lib/theme-sass/1_base_html/_index.css.js";
+import baseClassStyles from '@ucd-lib/theme-sass/2_base_class/_index.css.js';
+import objectsStyles from '@ucd-lib/theme-sass/3_objects/_index.css.js';
+import componentStyles from "@ucd-lib/theme-sass/4_component/_index.css.js";
+import layoutStyles from '@ucd-lib/theme-sass/5_layout/_index.css.js';
+import utilityStyles from '@ucd-lib/theme-sass/6_utility/_index.css.js';
 
 export function styles() {
     const elementStyles = css`
         :host {
             display: block;
         }
+        .center-container {
+            display: flex; 
+            justify-content: center; 
+            align-items: center;
+        }
     `;
 
-    return [elementStyles];
+    return [    
+        baseStyles,
+        componentStyles,
+        baseClassStyles,
+        objectsStyles,
+        layoutStyles,
+        utilityStyles,
+        elementStyles];
 }
 
 export function render() {
     return html`
-        <h1>Cookie Manager Elements</h1>
-        <p>This is where the cookie manager elements will go.</p>
-        <p>In progress. This is where the elements will be implemented.</p>
+
+        <h1>Cookies</h1>
+
+        <div class="responsive-table" role="region" aria-label="Scrollable Table" tabindex="0">
+            <table class="table--admin">
+                <thead>
+                    <tr>
+                        <th class="center-container">Delete?</th>
+                        <th>Cookie Name</th>
+                        <th>Cookie Length</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${this.cookies.map(cookie => 
+                        html`
+                            <tr>
+                                <td @click=${() => this.deleteCookie(cookie)} class="center-container">      
+                                   <p style="background-color: #c10230; border: 1px solid red; color: white;" class="btn btn--sm">Delete</p>
+                                </td>
+                                <td>${cookie.name}</td>
+                                <td>${cookie.valueLength}</td>
+                            </tr>`
+                        )}
+                </tbody>
+            </table>
+        </div>
+        
     `;
 }
